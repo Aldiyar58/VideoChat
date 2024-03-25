931e06fe52c2122d7fc64f2e03e7b9b8e5cd1b18
Алдияр Уалиев <Aldiyar58>
2024-03-25 15:00:50 +0600

    return heroku db

--- server.py
+++ server.py
@@ -12,37 +12,37 @@ CORS(app)
 app.config['SECRET_KEY'] = "thisismys3cr3tk3yrree"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://udamf8l8aitfqi:p574879a15710395d4743c7e23911c3a07742edd33b8b3d1fe03306f73e6ab73f@cb4l59cdg4fg1k.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d33oq7f779gjcd'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#
-# db = SQLAlchemy(app)
-#
-#
-# class Room(db.Model):
-#     __tablename__ = 'room'
-#     room_id = db.Column(db.String(), primary_key=True)
-#     username = db.Column(db.String(), primary_key=True)
-#     language = db.Column(db.String(), primary_key=True)
-#     language_level = db.Column(db.Integer(), primary_key=True)
-#
-#     def __repr__(self):
-#         return f'<Room "{self.room_id}"'
-#
-#     @classmethod
-#     def find_suitable_room(cls, language, language_level):
-#         if language == 'kaz':
-#             need_language = 'eng'
-#         else:
-#             need_language = 'kaz'
-#
-#         room = cls.query.filter_by(language=need_language, language_level=language_level).first()
-#         room.delete()
-#         return room
-#     def save(self):
-#         db.session.add(self)
-#         db.session.commit()
-#
-#     def delete(self):
-#         db.session.delete(self)
-#         db.session.commit()
+
+db = SQLAlchemy(app)
+
+
+class Room(db.Model):
+    __tablename__ = 'room'
+    room_id = db.Column(db.String(), primary_key=True)
+    username = db.Column(db.String(), primary_key=True)
+    language = db.Column(db.String(), primary_key=True)
+    language_level = db.Column(db.Integer(), primary_key=True)
+
+    def __repr__(self):
+        return f'<Room "{self.room_id}"'
+
+    @classmethod
+    def find_suitable_room(cls, language, language_level):
+        if language == 'kaz':
+            need_language = 'eng'
+        else:
+            need_language = 'kaz'
+
+        room = cls.query.filter_by(language=need_language, language_level=language_level).first()
+        room.delete()
+        return room
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
 
 socketio = SocketIO(app)
@@ -143,8 +143,8 @@ def on_data(data):
 
 
 if __name__ == "__main__":
-    # with app.app_context():
-    #     db.drop_all()
-    # with app.app_context():
-    #     db.create_all()
+    with app.app_context():
+        db.drop_all()
+    with app.app_context():
+        db.create_all()
     socketio.run(app, debug=True)
